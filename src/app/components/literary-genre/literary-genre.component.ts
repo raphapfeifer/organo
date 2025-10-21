591eb5f8ba26090eab4646b19bb32b99cc020387
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2025-10-21 00:42:20 -0300

    Final part of the project

--- src/app/components/literary-genre/literary-genre.component.ts
+++ src/app/components/literary-genre/literary-genre.component.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { BookComponent } from '../book/book.component';
 import { LiteraryGenre } from '../book/book';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-literary-genre',
-  imports: [BookComponent],
+  imports: [BookComponent,
+            CommonModule
+    ],
   templateUrl: './literary-genre.component.html',
   styleUrl: './literary-genre.component.css'
 })
